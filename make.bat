27d7e738cb0af22e255ee7f083bf33304462b3d5
Nurudin Imsirovic <realnurudinimsirovic@gmail.com>
2024-10-12 22:43:28 +0200

    provide shell scripts for windows so we don't rely on bash from wsl/msys2

--- make.bat
+++ make.bat
@@ -1,1 +1,5 @@
-@"C:\msys64\usr\bin\bash.exe" make.sh %*
+@echo off
+rd /s /q public
+mkdir public
+php gen-pages.php
+xcopy /s /e static\* public >nul 2>&1
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+# Local web server via PHP
+php -S 0.0.0.0:4444 -t public/
